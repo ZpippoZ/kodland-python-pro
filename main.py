03e7e0a16427c1ab5aba452310900ae12972c725
zPippo <111285407+ZpippoZ@users.noreply.github.com>
2024-01-09 19:33:32 +0100

    Updated main.py
    added kodland's platform code

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("main file, empty atm"
+meme_dict = {
+    "LOL": "una risposta a qualcosa di divertente",
+    "CRINGE": "qualcosa di strano o imbarazzante",
+    "SHEESH": "leggera disapprovazione",
+    "CREEPY": "spaventoso, inquietante",
+    "CHILL": "tranquillo",
+    "SNITCH": "spia",
+    "SNITCHARE": "fare la spia",
+    "BOOMER": "generalmente 40-50 anni o più, persone che hanno difficoltà ad usare le \"tecnologie moderne\"" 
+}
+
+print("Ciao boomer, benvenuto!")
+
+for i in range(5):
+    word = input("Scrivi un neologismo che non conosci\n").upper()
+    print(meme_dict[word]) if word in meme_dict.keys() else print("O non esiste, o non è nel nostro dizionario. Sorry!")
+    
+print("bye bye boomer")
