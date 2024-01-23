1ff8567ff8629b15cf3011e6e276c6b04a8c73ac
zPippo <111285407+ZpippoZ@users.noreply.github.com>
2024-01-23 20:12:31 +0100

    Updated bot_logic.py
    Added flip_a_coin() and random_emoji() functions

--- discord bot/bot_logic.py
+++ discord bot/bot_logic.py
@@ -6,3 +6,33 @@ chars = string.ascii_letters + string.digits + string.punctuation
 def gen_pass(pass_length):
     pw = "".join(choice(chars) for _ in range(pass_length))
     return pw
+
+def flip_a_coin():
+    if randint(0,1):
+        return "heads"
+    else:
+        return "tails"
+    
+emoji_dict = [
+    ":wave:",
+    ":smile:",
+    ":+1:",
+    ":skull:",
+    ":sunglasses:",
+    ":billed_cap:",
+    ":partying_face:",
+    ":thinking:",
+    ":eyes:",
+    ":relieved:",
+    ":joy:",
+    ":flushed:",
+    ":sob:",
+    ":cry:",
+    ":clown:",
+    ":heart:",
+    ":smiley:",
+    ":fire:"
+]
+
+def random_emoji():
+    return choice(emoji_dict)
